3d85cff6b91e63ea492b1c7b9ce6a0f261a126d1
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2021-01-25 06:25:31 +0700

    add config lithops to variable

---
+++ test_lithops_dag.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+from builtins import range
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import DAG
+from datetime import datetime, timedelta
+
+import time
+from pprint import pprint
+
+from word_processing import lithops_func
+
+seven_days_ago = datetime.combine(
+        datetime.today() - timedelta(7), datetime.min.time())
+
+args = {
+    'owner': 'airflow',
+    'start_date': seven_days_ago,
+}
+
+dag = DAG(
+    dag_id='word_processing_dag', default_args=args,
+    schedule_interval=None)
+
+
+
+run_this = PythonOperator(
+    task_id='word_processing',
+    python_callable=lithops_func,
+    op_args=['World'],
+    dag=dag)
+
+run_this
---
+++ word_processing.py
@@ -0,0 +1,43 @@
+import lithops
+from airflow.models import Variable
+
+# Dataset from: https://archive.ics.uci.edu/ml/datasets/bag+of+words
+iterdata = ['https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.enron.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.kos.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nips.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.nytimes.txt',
+            'https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/vocab.pubmed.txt']
+
+def my_map_function(url):
+    print('I am processing the object from {}'.format(url.path))
+    counter = {}
+
+    data = url.data_stream.read()
+
+    for line in data.splitlines():
+        for word in line.decode('utf-8').split():
+            if word not in counter:
+                counter[word] = 1
+            else:
+                counter[word] += 1
+
+    return counter
+
+
+def my_reduce_function(results):
+    final_result = {}
+    for count in results:
+        for word in count:
+            if word not in final_result:
+                final_result[word] = count[word]
+            else:
+                final_result[word] += count[word]
+
+    return final_result
+
+def lithops_func(*op_args):
+    config = Variable.get("lithops_config")
+    fexec = lithops.FunctionExecutor(config=config)
+    fexec.map_reduce(my_map_function, iterdata, my_reduce_function)
+    result = fexec.get_result()
+    print("Done! Result >> ",result)
