102365546abb5c8104b4579a33ec807cea0642bc
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2021-01-25 23:10:38 +0700

    change to map

--- lithops_copy.py
+++ lithops_copy.py
@@ -38,5 +38,5 @@ def lithops_run(*op_args):
     config = op_args[0]
     print(config)
     fexec = lithops.FunctionExecutor(config=config)
-    fexec.call_async(copy,op_args[1])
-    fexec.get_result()
+    result = fexec.map(copy,[op_args[1]])
+    print(result)
