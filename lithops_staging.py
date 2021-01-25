008966dcebd27bd4192825aa8f960863a9701e4f
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2021-01-26 06:02:58 +0700

    add pg

--- lithops_staging.py
+++ lithops_staging.py
@@ -1,6 +1,7 @@
 import lithops
 from MySQLdb import _mysql
 import pandas as pd
+import psycopg2
 
 def working(param):
     print(param)
@@ -13,8 +14,17 @@ def working(param):
     df = pd.DataFrame(user_result)
     print(df.head())
 
+    pgserver = '49ec7436-5643-423b-b0e4-158df3ec8b98.bqfh4fpt0vhjh7rs4ot0.databases.appdomain.cloud'
+    pguser = 'ibm_cloud_cb7d01ec_dcac_47ec_8d74_52635347bb1c'
+    pgpassword = '615b50b8183aadb25407cdc48e2e05e7a36300815649ae3c0e96fddc90ff00cf'
+    pgport = '31369'
+    pgdb = 'ibmclouddb'
 
-    return "Hello staging " + param
+    pgconn = psycopg2.connect(user=pguser,password=pgpassword,host=pgserver,database=pgdb,port=pgport)
+    pgcursor = pgconn.cursor()
+    print(pgconn.get_dsn_parameters(),"\n")
+
+    return "Hello mysql pandas pgsql "
 
 def run_lithops(*args):
     # print("hello")
