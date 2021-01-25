1e956e421dfca0301a92aeb7ca913d3f168b63b1
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2021-01-26 06:30:25 +0700

    add staging trx

--- copy_db_dag.py
+++ copy_db_dag.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 import time
 from pprint import pprint
 from staging_user import run_staging_user
+from staging_trx import run_staging_trx
 
 seven_days_ago = datetime.combine(
         datetime.today() - timedelta(7), datetime.min.time())
@@ -27,9 +28,15 @@ staging_pass = Variable.get("PG_STAGING_PASSWORD")
 # staging_pass = "test"
 
 staging_user_step = PythonOperator(
-    task_id='copy_db_dag',
+    task_id='copy_user_to_staging',
     python_callable=run_staging_user,
     op_args=[config,staging_pass],
     dag=dag)
 
-staging_user_step
+staging_trx_step = PythonOperator(
+    task_id='copy_trx_to_staging',
+    python_callable=run_staging_trx,
+    op_args=[config,staging_pass],
+    dag=dag)
+
+staging_user_step >> staging_trx_step
---
+++ staging_trx.py
@@ -0,0 +1,47 @@
+import lithops
+from MySQLdb import _mysql
+import pandas as pd
+import psycopg2
+
+def working(param):
+    print(param)
+    db=_mysql.connect(host="cap-au-sg-prd-05.securegateway.appdomain.cloud",port=15208,user="habib",passwd="password123",db="testdev")
+    db.query("""SELECT * FROM trx""")
+    r = db.store_result()
+    trx_result = r.fetch_row(maxrows=100,how=1)
+    print("success load data from mysql")
+
+    df = pd.DataFrame(trx_result)
+    print(df.head())
+
+    pgserver = '49ec7436-5643-423b-b0e4-158df3ec8b98.bqfh4fpt0vhjh7rs4ot0.databases.appdomain.cloud'
+    pguser = 'ibm_cloud_cb7d01ec_dcac_47ec_8d74_52635347bb1c'
+    pgpassword = param
+    pgport = '31369'
+    pgdb = 'ibmclouddb'
+
+    pgconn = psycopg2.connect(user=pguser,password=pgpassword,host=pgserver,database=pgdb,port=pgport)
+    pgcursor = pgconn.cursor()
+    print(pgconn.get_dsn_parameters(),"\n")
+
+
+    query_insert = """ INSERT INTO public.t_trx(trx_id,username,item_id,qty,amount) values (%s,%s,%s,%s,%s) """
+    
+    for index,row in df.iterrows():
+        record = (row['trx_id'].decode('utf-8'),
+                row['username'].decode('utf-8'),
+                row['item_id'].decode('utf-8'),
+                row['qty'].decode('utf-8'),
+                row['amount'].decode('utf-8'))
+        pgcursor.execute(query_insert,record)
+        
+    
+    pgconn.commit()
+    return "Success staging trx"
+
+def run_staging_trx(*args):
+    # print("hello")
+    # config = Variable.get("lithops_config", deserialize_json=True)
+    fexec = lithops.FunctionExecutor(config=args[0])
+    fexec.call_async(working,args[1])
+    print(fexec.get_result())
