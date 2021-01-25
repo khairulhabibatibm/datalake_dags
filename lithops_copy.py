900bc904e586b724a9c56b29dcd8b37e467ab21e
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2021-01-26 05:16:14 +0700

    debug

--- lithops_copy.py
+++ lithops_copy.py
@@ -1,6 +1,6 @@
 import lithops
 # from MySQLdb import _mysql
-import psycopg2
+# import psycopg2
 # import pandas as pd
 
 def copy(staging_pass):
@@ -14,19 +14,19 @@ def copy(staging_pass):
     # for index,row in df.iterrows():
     #     print(row['username'].decode('utf-8'), row['dob'].decode('utf-8'))
 
-    print("without mysql and pandas")
+    print("without mysql and pandas and postgres")
 
-    pgserver = '49ec7436-5643-423b-b0e4-158df3ec8b98.bqfh4fpt0vhjh7rs4ot0.databases.appdomain.cloud'
-    pguser = 'ibm_cloud_cb7d01ec_dcac_47ec_8d74_52635347bb1c'
-    pgpassword = staging_pass
-    pgport = '31369'
-    pgdb = 'ibmclouddb'
+    # pgserver = '49ec7436-5643-423b-b0e4-158df3ec8b98.bqfh4fpt0vhjh7rs4ot0.databases.appdomain.cloud'
+    # pguser = 'ibm_cloud_cb7d01ec_dcac_47ec_8d74_52635347bb1c'
+    # pgpassword = staging_pass
+    # pgport = '31369'
+    # pgdb = 'ibmclouddb'
 
-    pgconn = psycopg2.connect(user=pguser,password=pgpassword,host=pgserver,database=pgdb,port=pgport)
-    pgcursor = pgconn.cursor()
-    print(pgconn.get_dsn_parameters(),"\n")
+    # pgconn = psycopg2.connect(user=pguser,password=pgpassword,host=pgserver,database=pgdb,port=pgport)
+    # pgcursor = pgconn.cursor()
+    # print(pgconn.get_dsn_parameters(),"\n")
 
-    query_insert = """ INSERT INTO public.t_user(username,job,dob,country) values (%s,%s,%s,%s) """
+    # query_insert = """ INSERT INTO public.t_user(username,job,dob,country) values (%s,%s,%s,%s) """
     
     # for index,row in df.iterrows():
     #     record = (row['username'].decode('utf-8'),row['occupation'].decode('utf-8'),row['dob'].decode('utf-8'),row['country'].decode('utf-8'))
