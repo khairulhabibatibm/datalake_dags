578a20c9f6353f8af4acda08a0e226dda2f97b6d
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2021-01-26 05:51:29 +0700

    add mysql

--- lithops_staging.py
+++ lithops_staging.py
@@ -1,8 +1,13 @@
 import lithops
+from MySQLdb import _mysql
 
 def working(param):
     print(param)
-    return "Hello staging " + param;
+    db=_mysql.connect(host="cap-au-sg-prd-05.securegateway.appdomain.cloud",port=15208,user="habib",passwd="password123",db="testdev")
+    db.query("""SELECT * FROM user""")
+    r = db.store_result()
+    user_result = r.fetch_row(maxrows=0,how=1)
+    return "Hello staging " + param
 
 def run_lithops(*args):
     # print("hello")
