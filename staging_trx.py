36af3a1a247d1cc3234283e9852b5da4bdd89409
Muhammad Khairul Habib Muttaqien <khairul.habib@ibm.com>
2021-01-26 10:52:16 +0700

    remove limitation

--- staging_trx.py
+++ staging_trx.py
@@ -10,7 +10,7 @@ def working(param,passwd):
     db=_mysql.connect(host="cap-au-sg-prd-05.securegateway.appdomain.cloud",port=15208,user="habib",passwd="password123",db="testdev")
     db.query("SELECT * FROM trx where trx_id = {0}".format(param))
     r = db.store_result()
-    trx_result = r.fetch_row(maxrows=10,how=1)
+    trx_result = r.fetch_row(maxrows=0,how=1)
     print("success load data from mysql")
 
     df = pd.DataFrame(trx_result)
